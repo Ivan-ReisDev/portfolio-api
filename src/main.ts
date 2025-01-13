2a7031b41b1164099c7c3c450a05f17304570238
Ivan Reis <ivanreisdev@gmail.com>
2025-01-13 14:45:14 -0300

    test: teste de produção

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'https://portfolio-theta-sepia-99.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
