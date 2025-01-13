8d3cd6fdaab2fc75ae5845f13ad023ce6e01bf9b
Ivan Reis <ivanreisdev@gmail.com>
2025-01-13 14:36:59 -0300

    test: teste de produção

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://portfolio-theta-sepia-99.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
 
   app.use(cookieParser());
