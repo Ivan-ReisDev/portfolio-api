1a412d48b34f4597d3df92b2a241f974a4b40394
Ivan Reis <ivanreisdev@gmail.com>
2025-01-15 22:00:57 -0300

    feat: adicionando workflow

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Acesse a documentação em /api-docs teste';
+    return 'Acesse a documentação em /api-docs';
   }
 }
